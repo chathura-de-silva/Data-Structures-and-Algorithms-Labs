e9a463d701f7a5a6805f13eefd9c039d281c0562
chathura7357 <97898278+chathura7357@users.noreply.github.com>
2023-05-04 15:01:12 +0530

    Update Heap -Inclass lab Week 8.cpp

--- Week 8/Heap -Inclass lab Week 8.cpp
+++ Week 8/Heap -Inclass lab Week 8.cpp
@@ -23,7 +23,7 @@ void heapSort(int arr[], int n){
     // First Builds the Max-Heap.
     for (int i = (n-1)/2 ; i>=0; i--)
         heapify(arr,n, i);
-//Then does the sorting process
+// Then does the sorting process
     for(int i=n-1;i>0;i--){
         swap(arr[i],arr[0]);
         heapify(arr,i,0); // when calling heapify, array size being reduced by 1 in each iteration via i.
